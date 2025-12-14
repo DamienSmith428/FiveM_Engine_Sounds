f71b31d610dd9b1ca9919f690870c63e7c15807e
Damien Smith <154563895+DamienSmith428@users.noreply.github.com>
2025-12-14 00:02:52 -0600

    Add files via upload

---
+++ Audio_Pack/fxmanifest.lua
@@ -0,0 +1,36 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'PrinceAlbert'
+description 'Custom Car Audio'
+
+author 'Damien428'
+description 'FiveM Engine Audio Pack'
+repository 'https://github.com/SpiritsCreations/FiveM-Engine-Sound-Pack/Audio_Pack'
+
+server_scripts {
+	"versioncheck.lua"
+}
+
+
+files {
+    '**/**/*.dat151.rel',
+    '**/**/*.dat54.rel',
+    '**/**/*.dat10.rel',
+    '**/**/*.awc',
+}
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/hb450s_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/hb450s_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_hb450s'
+
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta488f154_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta488f154_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta488f154'
+
+data_file 'AUDIO_GAMEDATA' 'audioconfig/ta103ninjah2r_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta103ninjah2r_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta103ninjah2r'
+
+
