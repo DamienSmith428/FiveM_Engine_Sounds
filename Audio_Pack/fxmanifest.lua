b9e4f93968bc85106ce2e957b410c60baf713f7e
Damien Smith <damiensmith499@gmail.com>
2025-12-14 01:09:48 -0600

    Fixed Manifest
    Fixed manifest

--- Audio_Pack/fxmanifest.lua
+++ Audio_Pack/fxmanifest.lua
@@ -16,18 +16,22 @@ files {
     '**/**/*.dat10.rel',
     '**/**/*.awc',
 }
-
+--Used by [hb450s]
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/hb450s_amp.dat'
 data_file 'AUDIO_GAMEDATA' 'audioconfig/hb450s_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/hb450s_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_hb450s'
-
-
+--Used by NONE
 data_file 'AUDIO_GAMEDATA' 'audioconfig/ta488f154_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta488f154_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta488f154'
-
+--Used by [nh2r]
 data_file 'AUDIO_GAMEDATA' 'audioconfig/ta103ninjah2r_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audioconfig/ta103ninjah2r_sounds.dat'
 data_file 'AUDIO_WAVEPACK' 'sfx/dlc_ta103ninjah2r'
-
+--Used by [c10custom]
+data_file 'AUDIO_SYNTHDATA' 'audioconfig/chevydmaxeng_amp.dat'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/chevydmaxeng_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/chevydmaxeng_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_chevydmaxeng'
 
